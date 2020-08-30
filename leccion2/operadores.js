6b957e137bf85c509410f67d4415e4dade550d92
martincrb <martincristobaltdr@gmail.com>
2020-08-30 14:25:03 +0200

    add numerical operators

---
+++ leccion2/operadores.js
@@ -0,0 +1,11 @@
+// Comentarios
+// operadores matematicos
+var suma = 3 + 3; //6
+var resta = 3 - 3; //0
+var mult = 3 * 3; // 9
+var divi = 3 / 3; // 1
+
+var resto = 3 % 4;
+var expo = 2 ** 3;
+
+console.log(expo);
---
+++ leccion2/variables.js
@@ -0,0 +1,6 @@
+var resultado1 = 3 + 3
+var resultado2 = 3
+
+console.log(resultado2)
+resultado2 = resultado1 + 1
+console.log(resultado2)
